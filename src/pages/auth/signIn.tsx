0d803f1bda66eaa7d5c30aa89da9e3bfdf846986
杨静波 <alex@defipay.biz>
2023-05-10 14:47:27 +0800

    feat：登录页面

--- src/pages/auth/signIn.tsx
+++ src/pages/auth/signIn.tsx
@@ -6,21 +6,43 @@ import { useTranslation } from 'react-i18next';
 export default function SignIn() {
     const { t } = useTranslation();
     const formRules = {
-        username: [{ required: true, message: t("message.pwd"), trigger: 'blur' }],
+        username: [{ required: true, message: t("message.account"), trigger: 'blur' }],
         password: [{ required: true, message: t("message.pwd"), trigger: 'blur' }],
     }
     const navigate = useNavigate();
-    const onFinish = (values: any) => {
+
+    function informationEcho() {
+        if (!localStorage?.getItem("userLoginInfo")) return []
+        return JSON.parse(localStorage?.getItem("userLoginInfo") as string)
+    }
+
+    function onFinish(values: any) {
+        if (values.remember) {
+            localStorage.setItem("userLoginInfo", JSON.stringify([
+                {
+                    name: "username",
+                    value: values.username,
+                },
+                {
+                    name: "remember",
+                    value: values.remember,
+                },
+            ]));
+        } else {
+            localStorage.removeItem("userLoginInfo");
+        }
         navigate("/layout/home")
     };
 
     return <>
         <div className="login_page">
-            <Card style={{ width: 500, margin: "0 auto" }}>
+            <Card style={{ width: 300, margin: "0 auto" }}>
 
-                <Button block={true} >Google 登录</Button>
+                <Button block={true} >Google {t("login.loginBtn")}</Button>
+
                 <Divider>Or</Divider>
-                <Form name="basic" layout="vertical" onFinish={onFinish}  >
+
+                <Form name="basic" layout="vertical" onFinish={onFinish} fields={informationEcho()}  >
                     <Form.Item label="用户名" name="username" rules={formRules["username"]}  >
                         <Input />
                     </Form.Item>
@@ -30,11 +52,11 @@ export default function SignIn() {
                     </Form.Item>
 
                     <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-                        <Checkbox>记住账号密码</Checkbox>
+                        <Checkbox>{t("login.rememberPwd")}</Checkbox>
                     </Form.Item>
 
                     <Form.Item>
-                        <Button type="primary" htmlType="submit" block>登录</Button>
+                        <Button type="primary" htmlType="submit" block>{t("login.loginBtn")}</Button>
                     </Form.Item>
                 </Form>
             </Card>
